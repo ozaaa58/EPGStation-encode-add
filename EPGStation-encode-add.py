0ecbfa1b973185d4279df83dedcb80d02fb3afbf
ozaaa58 <falcon.kc.58@gmail.com>
2023-10-01 01:42:24 +0900

    リファクタ

--- EPGStation-encode-add.py
+++ EPGStation-encode-add.py
@@ -1,80 +1,110 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[69]:
-
-
 import requests
 import json
 import datetime
 import codecs
+import yaml
+import logging
+
+# configファイルの読み込み
+with open("config.yaml", "r") as yml:
+    config: dict = yaml.safe_load(yml)
+
+# サーバーアドレスを取得
+server_addr: str = config["server_addr"]
+
+# エンコードオプションを取得
+enc_option = config["enc_option"]
+# directoryオプションが設定されていない場合は要素を削除
+if enc_option["directory"] == "None":
+    enc_option.pop("directory")
+
+# ログの設定
+logging.basicConfig(
+    level=config["log_level"],
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    filename="app.log",
+    filemode="a",  # ログの出力モード ('w': 上書き, 'a': 追記)
+)
+
+# ログ出力インスタンスを作成する。引数にログ出力名称を設定する。
+logger = logging.getLogger("Logging")
+
+# ログを標準出力する。
+sh = logging.StreamHandler()
+logger.addHandler(sh)
 
 
-# In[70]:
+# エンコードキューへ追加する関数
+def enc(rec_id, vid_id, enc_option):
+    enc_api_url = server_addr + "/api/encode"  # エンコードAPI-URLを設定
 
+    # IDを設定
+    enc_option.update(recordedId=rec_id, sourceVideoFileId=vid_id)
 
-server_addr = "http://localhost:8888"  #サーバーアドレス
-
-def enc(rec_id,vid_id):
-    enc_api_url = server_addr + "/api/encode"
-    enc_option = {"recordedId": rec_id,
-                  "sourceVideoFileId": vid_id,
-                  "parentDir": "encoded-2",
-                  #"directory": "string",
-                  "isSaveSameDirectory": False,
-                  "mode": "H.264",
-                  "removeOriginal": False}  #エンコード設定
-
+    # APIからエンコードキューに追加
     enc_api = requests.post(enc_api_url, json=enc_option)
     enc_add_result = enc_api.json()
-    if'encodeId' in enc_add_result:
-        print("Success! :",enc_add_result)
-        print("Success! :",enc_add_result,end="\n", file=codecs.open('log.txt', 'a', 'utf-8'))  #ログをテキストで出力不要な場合はコメントアウト
 
+    # 結果の確認
+    if "encodeId" in enc_add_result:
+        logger.info(f"Success! : {enc_add_result}")
+        result_code = 0
     else:
-        print("Failure! :",enc_add_result)
-        print("Failure! :",enc_add_result,end="\n", file=codecs.open('log.txt', 'a', 'utf-8'))  #ログをテキストで出力不要な場合はコメントアウト
+        logger.error(f"Failure! : {enc_add_result}")
+        result_code = 1
 
-print(datetime.datetime.now())
-print(datetime.datetime.now(),end="\n", file=codecs.open('log.txt', 'a', 'utf-8'))
+    return result_code
 
-enc_cue_api = server_addr + "/api/encode?isHalfWidth=true"  #エンコードキュー情報を取得
+
+# エンコードキュー情報を取得
+enc_cue_api = server_addr + "/api/encode?isHalfWidth=true"
 enc_cue = requests.get(enc_cue_api)
 enc_cue_json = enc_cue.json()
 
-rec_list_api = server_addr + "/api/recorded?isHalfWidth=true&offset=0&limit=30"  #録画済み情報取得。現状は最新30件分を取得
+# 録画リスト情報を取得
+rec_list_api = (
+    server_addr
+    + "/api/recorded?isHalfWidth=true&offset=0&limit="
+    + str(config["limit"])
+)
 rec_list = requests.get(rec_list_api)
 rec_list_json = rec_list.json()
 
-in_progress = []
-
+# 既にエンコードキューにあるのプログラムIDを取得
+in_progress: list = []
 if len(enc_cue_json["runningItems"]) != 0:
     in_progress.append(enc_cue_json["runningItems"][0]["recorded"]["id"])
     if len(enc_cue_json["waitItems"]) != 0:
         for s in range(len(enc_cue_json["waitItems"])):
             in_progress.append(enc_cue_json["waitItems"][s]["recorded"]["id"])
 
+# エンコードキューに追加するプログラムIDを取得
 for i in range(len(rec_list_json["records"])):
     program_info = rec_list_json["records"][i]
     rec_file_info = program_info["videoFiles"]
 
+    # videoFilesの要素数で処理を分岐
+    # 要素数が2つ以上の場合はエンコードファイルが存在している可能性が高いため処理しない
     if len(rec_file_info) == 1:
         file_type = rec_file_info[0]["type"]
+        # FileTypeがTSかどうかを確認
         if file_type == "ts":
             rec_id = program_info["id"]
             vid_id = rec_file_info[0]["id"]
             if len(in_progress) != 0:
                 if rec_id in in_progress:
-                    print("ProgramID:",rec_id," This ProgramID is already in the encode cue")
-                    print("ProgramID:",rec_id," This ProgramID is already in the encode cue",end="\n", file=codecs.open('log.txt', 'a', 'utf-8'))  #ログをテキストで出力不要な場合はコメントアウト
+                    logger.info(
+                        f"ProgramID:{rec_id} This ProgramID is already in the encode cue"
+                    )
                 else:
-                    print("ProgramID:",rec_id,"VideoID:",vid_id,"is encode-add")
-                    print("ProgramID:",rec_id,"VideoID:",vid_id,"is encode-add",end="\n", file=codecs.open('log.txt', 'a', 'utf-8'))  #ログをテキストで出力不要な場合はコメントアウト
-                    enc(rec_id,vid_id)
+                    logger.info(f"ProgramID: {rec_id} VideoID: {vid_id} is encode-add")
+                    enc(rec_id, vid_id, enc_option)
             else:
-                print("ProgramID:",rec_id,"VideoID:",vid_id,"is encode-add")
-                print("ProgramID:",rec_id,"VideoID:",vid_id,"is encode-add",end="\n", file=codecs.open('log.txt', 'a', 'utf-8'))  #ログをテキストで出力不要な場合はコメントアウト
-                enc(rec_id,vid_id)
+                logger.info(f"ProgramID: {rec_id} VideoID: {vid_id} is encode-add")
+                enc(rec_id, vid_id, enc_option)
 
         else:
             pass
@@ -89,10 +119,3 @@ for i in range(len(rec_list_json["records"])):
 #            print("no-encode")
 #        else:
 #            pass
-
-
-# In[ ]:
-
-
-
-
--- README.md
+++ README.md
@@ -1,22 +1,17 @@
 # EPGStation-encode-add
-PythonとGitの勉強のために作ったもので、バグがあるかもしれません。
 
-EPGStationのAPIを利用し、録画済み情報を取得、エンコードされていないものを
+Python と Git の勉強のために作ったもので、バグがあるかもしれません。
+
+EPGStation の API を利用し、録画済み情報を取得、エンコードされていないものを
 エンコードキューへ追加していくものです。
-cron等に登録して深夜帯や日中にまとめてエンコードしたい場合に良いかと・・・
+cron 等に登録して深夜帯や日中にまとめてエンコードしたい場合に。
 
-サーバーアドレス・エンコード情報など自分の環境に合わせ書き換えてください。
+サーバーアドレス・エンコード情報など自分の環境に合わせ `config.yaml` 書き換えてください。
 
-http://< server_addr >:< port >/api-docs/?url=/api/docs
+`http://< server_addr >:< port >/api-docs/?url=/api/docs`
 
-上記URLでEPGStationのAPIが確認できます。各自必要に応じて参照願います。
+上記 URL で EPGStation の API が確認できます。各自必要に応じて参照願います。
 
-WebAPIを叩くため”requests”ライブラリが必要です。
+WebAPI を叩くため `requests` ライブラリが必要です。
 
-pip install requests
-
-or
-
-conda install requests
-
-でライブラリをインストールしてください。
+`pip install requests` or `conda install requests` でライブラリをインストールしてください。
